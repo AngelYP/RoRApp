46cdc1b2abf3cda7518b237f8d45b693c357ba0a
Angel Yáñez <angel.yaperez@gmail.com>
2020-07-28 15:08:59 -0500

    date added, extras deleted, details fixed

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,2 +1,8 @@
 class Daily < ApplicationRecord
+
+    validates :positives, presence:true
+    validates :deaths, presence:true
+    validates :suspects, presence:true
+    validates :negatives, presence:true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Daily.create(
-    created_at:"2020-07-15 11:04:44 UTC",
-    positives:1073,
-    deaths:74,
-    suspects:231,
-    negatives:2644
-)
 
 Daily.create(
     created_at:"2020-07-16 11:04:44 UTC",
-    positives:1099,
+    positives:1026,
     deaths:77,
     suspects:238,
     negatives:2683
@@ -24,7 +17,7 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-17 11:04:44 UTC",
-    positives:1127,
+    positives:1052,
     deaths:79,
     suspects:256,
     negatives:2736
@@ -32,7 +25,7 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-18 11:04:44 UTC",
-    positives:1154,
+    positives:1074,
     deaths:82,
     suspects:272,
     negatives:2785
@@ -40,7 +33,7 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-19 11:04:44 UTC",
-    positives:1186,
+    positives:1109,
     deaths:87,
     suspects:275,
     negatives:2819
@@ -48,7 +41,7 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-20 11:04:44 UTC",
-    positives:1206,
+    positives:1114,
     deaths:87,
     suspects:276,
     negatives:2843
@@ -56,7 +49,7 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-21 11:04:44 UTC",
-    positives:1225,
+    positives:1145,
     deaths:88,
     suspects:281,
     negatives:2863
@@ -64,7 +57,7 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-22 11:04:44 UTC",
-    positives:1258,
+    positives:1188,
     deaths:88,
     suspects:287,
     negatives:2922
@@ -72,7 +65,7 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-23 11:04:44 UTC",
-    positives:1296,
+    positives:1229,
     deaths:88,
     suspects:291,
     negatives:2975
@@ -80,7 +73,7 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-24 11:04:44 UTC",
-    positives:1322,
+    positives:1260,
     deaths:89,
     suspects:319,
     negatives:2644
@@ -88,8 +81,24 @@ Daily.create(
 
 Daily.create(
     created_at:"2020-07-25 11:04:44 UTC",
-    positives:1325,
+    positives:1289,
     deaths:90,
+    suspects:433,
+    negatives:3023
+)
+
+Daily.create(
+    created_at:"2020-07-26 11:04:44 UTC",
+    positives:1308,
+    deaths:93,
+    suspects:433,
+    negatives:3023
+)
+
+Daily.create(
+    created_at:"2020-07-27 11:04:44 UTC",
+    positives:1315,
+    deaths:94,
     suspects:433,
     negatives:3023
 )
