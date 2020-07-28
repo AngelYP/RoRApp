fb17fcfaa2d985e6a369f8360943ee36dc08c791
Angel Yáñez <angel.yaperez@gmail.com>
2020-07-27 21:01:14 -0500

    General table at home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,9 @@ class HomeController < ApplicationController
 
 
     def showData
-        @cases=Daily.all
+        @headers=["Día","Confirmados","Muertes","Sospechosos","Negativos"]
+        @cases=Daily.order(id: :desc).take(5)
+        @last10=Daily.find(Daily.last.id-10).positives
     end
 
 end
